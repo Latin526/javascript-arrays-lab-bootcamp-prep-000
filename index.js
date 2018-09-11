d13307ea125a6239a70650b662c3b8a1454d1b59
yasmintorres90@hotmail.com <yasmintorres90@hotmail.com>
2018-09-11 13:39:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
 }
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length -1);
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1);
+  return newArray;
+}
