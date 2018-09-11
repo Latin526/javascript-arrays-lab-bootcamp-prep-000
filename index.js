195dfe416160d887111a7f84878bc11c76511649
yasmintorres90@hotmail.com <yasmintorres90@hotmail.com>
2018-09-11 13:33:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Add your functions and code here
-const app = 'I don't do much.""
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kittenName) {
+  kittens.push(kittenName);
+  return kittens;
+}
